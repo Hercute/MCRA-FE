3126d81222e9989dfd64b46d885610a59af752a3
kimsohyeon <aotoyae@gmail.com>
2024-07-02 22:30:45 +0900

    [feat] tanstack query 세팅

--- src/app/RecoilRootWrapper.tsx
+++ src/app/RecoilRootWrapper.tsx
@@ -1,9 +1,14 @@
 'use client';
 
 import { RecoilRoot } from 'recoil';
+import QueryProvider from './provider';
 
 const RecoilRootWrapper = ({ children }: { children: React.ReactNode }) => {
-  return <RecoilRoot>{children}</RecoilRoot>;
+  return (
+    <QueryProvider>
+      <RecoilRoot>{children}</RecoilRoot>
+    </QueryProvider>
+  );
 };
 
 export default RecoilRootWrapper;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
-import RecoilRootWrapper from '@/components/layout/RecoilRootWrapper';
+import RecoilRootWrapper from './RecoilRootWrapper';
 
 import type { Metadata } from 'next';
 
