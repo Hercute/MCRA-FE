c51241a0354b3cc4a00a36ade02a264c9bd2c93c
Kim CG <cjfrbs1256@gmail.com>
2024-07-04 03:08:12 +0900

    [feat]#1:레시피 소제목 공통컴포넌트 제작중

--- src/app/recipe/AuthRecipeList.tsx
+++ src/app/recipe/AuthRecipeList.tsx
@@ -1,6 +1,8 @@
 'use client';
 import React from 'react';
 import { Recipe } from '../data/recipeDummyData';
+import { FaRegThumbsUp } from 'react-icons/fa';
+import './AuthRecipeList.css';
 
 type AuthRecipeListProps = {
   recipes: Recipe[];
@@ -10,10 +12,20 @@ const AuthRecipeList: React.FC<AuthRecipeListProps> = ({ recipes }) => {
   return (
     <div>
       {AuthRecipes.map((recipe) => (
-        <div key={recipe.id}>
-          <p>{recipe.name}</p>
-          <p>{recipe.likeCounts}</p>
-        </div>
+        <>
+          <div className="subTitle">
+            <h2>
+              <FaRegThumbsUp />
+              <span>메추리픽 레시피</span>
+            </h2>
+            <span className="subExplanation">메추리알 운영진의 찐레시피</span>
+            <button type="button">더보기</button>
+          </div>
+          <div key={recipe.id}>
+            <p>{recipe.name}</p>
+            <p>{recipe.likeCounts}</p>
+          </div>
+        </>
       ))}
     </div>
   );
