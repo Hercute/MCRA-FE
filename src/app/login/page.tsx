fa6f6960d39a1c4124587ccf8e9667514d6ebad3
kimsohyeon <aotoyae@gmail.com>
2024-07-09 23:24:42 +0900

    [feat] #8 로그인 폼 제작

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,15 @@
 const LoginPage = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <form action="">
+        <div>logo</div>
+        <input type="text" />
+        <button>계속</button>
+      </form>
+      <button>구글로 로그인</button>
+      <button>회원가입</button>
+    </div>
+  );
 };
 
 export default LoginPage;
