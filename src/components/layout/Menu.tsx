563c1b4692e31c94f695a277505ab02e5ce1aef4
Seoweol <ahddl622@naver.com>
2024-07-10 18:40:38 +0900

    [style] #11 하단 메뉴바 CSS

--- src/components/layout/Menu.tsx
+++ src/components/layout/Menu.tsx
@@ -1,23 +1,26 @@
 import Link from 'next/link';
+import './style.css';
 
 const Menu = () => {
-  return <section>
-  <Link href='/'>
-  <h4>홈</h4>
-  </Link>
-  <Link href='/recipe'>
-  <h4>레시피</h4>
-  </Link>
-  <Link href='/fridge'>
-  <h4>냉장고</h4>
-  </Link>
-  <Link href='/cart'>
-  <h4>장보기</h4>
-  </Link>
-  <Link href='/login'>
-  <h4>로그인</h4>
-  </Link>
-</section>;
+  return (
+    <section className='menuSection'>
+      <Link href="/">
+        <h4>홈</h4>
+      </Link>
+      <Link href="/recipe">
+        <h4>레시피</h4>
+      </Link>
+      <Link href="/fridge">
+        <h4>냉장고</h4>
+      </Link>
+      <Link href="/cart">
+        <h4>장보기</h4>
+      </Link>
+      <Link href="/login">
+        <h4>로그인</h4>
+      </Link>
+    </section>
+  );
 };
 
 export default Menu;
