9164c4f06fcb20444257bd0f3b548a84a63638d9
kcg <cjfrbs1256@gmail.com>
2024-07-11 14:36:00 +0900

    [feat]#14:해시태그input생성및 atom생성

--- src/app/recipe/upload/HashTag.tsx
+++ src/app/recipe/upload/HashTag.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
+'use client';
+import { hashTagsState } from '@/recoil/atoms/recoilState';
+import React, { useState } from 'react';
 import { IoBookOutline } from 'react-icons/io5';
+import { useRecoilState } from 'recoil';
 
 const HashTag = () => {
+  const [tagItem, setTagItem] = useState('');
+  const [hashTags, setHashTags] = useRecoilState(hashTagsState);
+
+  const addHashTag = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const newHashTag = tagItem.trim();
+    setHashTags((prevHashTags) => {
+      return [...new Set([...prevHashTags, newHashTag])];
+    });
+  };
   return (
     <section className="recipeSection">
       <div className="uploadDiv">
         <IoBookOutline size={27} />
         <span>해시태그</span>
       </div>
-      HashTag
+      <div className="hashTagBox">
+        <input
+          placeholder="태그를 입력하세요"
+          value={tagItem}
+          onChange={(e) => setTagItem(e.target.value)}
+          onKeyUp={addHashTag}
+        />
+      </div>
     </section>
   );
 };
