f93a4c828bfabc112d964974a94a43cb41826946
kimsohyeon <aotoyae@gmail.com>
2024-07-12 17:42:58 +0900

    [feat] #23 재료 추가 페이지 이동 Link

---
+++ src/app/fridge/food/page.tsx
@@ -0,0 +1,44 @@
+const FridgeAddPage = () => {
+  return (
+    <ul>
+      <li>
+        <div>img</div>
+        <h4>야채</h4>
+      </li>
+      <li>
+        <div>img</div>
+        <h4>과일</h4>
+      </li>
+      <li>
+        <div>img</div>
+        <h4>육류</h4>
+      </li>
+      <li>
+        <div>img</div>
+        <h4>해물</h4>
+      </li>
+      <li>
+        <div>img</div>
+        <h4>곡류</h4>
+      </li>
+      <li>
+        <div>img</div>
+        <h4>조미료</h4>
+      </li>
+      <li>
+        <div>img</div>
+        <h4>가공식품</h4>
+      </li>
+      <li>
+        <div>img</div>
+        <h4>음료</h4>
+      </li>
+      <li>
+        <div>img</div>
+        <h4>기타</h4>
+      </li>
+    </ul>
+  );
+};
+
+export default FridgeAddPage;
--- src/app/fridge/page.tsx
+++ src/app/fridge/page.tsx
@@ -1,5 +1,14 @@
+import Link from 'next/link';
+
 const FridgePage = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      page
+      <Link href="fridge/food">
+        <button>냉장고 채우기</button>
+      </Link>
+    </div>
+  );
 };
 
 export default FridgePage;
