df8b32df70ac10453e4aea38c6eaf0af766f37af
kcg <cjfrbs1256@gmail.com>
2024-07-12 17:22:44 +0900

    [feat]#20:조리순서-레시피설명 저장및 미리보기

--- src/app/recipe/upload/RecipeOrder.tsx
+++ src/app/recipe/upload/RecipeOrder.tsx
@@ -7,6 +7,7 @@ import { useRecoilState } from 'recoil';
 import { orderPagesState } from '@/recoil/atoms/recoilState';
 
 import './upload.css';
+import Image from 'next/image';
 
 const RecipeOrder: React.FC = () => {
   const [recipePage, setRecipePage] = useRecoilState(orderPagesState);
@@ -40,6 +41,9 @@ const RecipeOrder: React.FC = () => {
     setRecipePage(updateOrder);
     console.log(recipePage);
   };
+  const handleDescription = (id: number, value: string) => {
+    setRecipePage((prevPages) => prevPages.map((page) => (page.id === id ? { ...page, description: value } : page)));
+  };
   return (
     <section className="recipeSection">
       <div className="uploadDiv">
@@ -49,12 +53,22 @@ const RecipeOrder: React.FC = () => {
       {recipePage.map((page, index) => (
         <div className="orderItem" key={page.id}>
           <div className="orderFileBox" onClick={handleDivClick}>
-            <AiOutlinePicture size={36} />
+            {filePreview ? (
+              <Image src={filePreview} alt={page.description} className="uploadedImage" width={80} height={80} />
+            ) : (
+              <AiOutlinePicture size={36} />
+            )}
+
             <input type="file" className="orderFile" ref={fileInputRef} onChange={handleFileChange} />
           </div>
           <div className="recipeExplanation">
-            <span>{index}</span>
-            <textarea placeholder="레시피설명"></textarea>
+            <span>{index + 1}</span>
+            <textarea
+              name="description"
+              value={page.description}
+              onChange={(e) => handleDescription(page.id, e.target.value)}
+              placeholder="레시피설명"
+            ></textarea>
           </div>
           {page.showDeleteButton && (
             <button className="deleteButton" onClick={() => handleDeleteOrder(page.id)}>
