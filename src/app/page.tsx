4e687bae325da2be10ea2fcb8f32539d87b57312
Seoweol <ahddl622@naver.com>
2024-07-16 23:41:41 +0900

    [feat] #11 메인

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
-    <main>
-      <h1>MCRA</h1>
+    <main style={{ padding: '0 0.5rem', width: '375px', border: '1px solid #999999', margin: '0 auto' }}>
+      <section>
+        <input type="text" placeholder="레시피 검색"/>
+      </section>
+      <div>
+        <button>장보러가기</button>
+        <button>냉장고</button>
+      </div>
     </main>
   );
 }
