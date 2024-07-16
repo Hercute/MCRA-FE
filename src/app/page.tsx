4387caa2ed92777d37db3a1c3ab6c52e7e545e2a
Seoweol <ahddl622@naver.com>
2024-07-17 01:37:12 +0900

    [feat] #29 메인페이지, 상단 헤더

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,24 @@
 import Image from 'next/image';
+
 import MainHeader from '@/components/layout/MainHeader';
 import buttonImage from '../assets/button/button.png';
 import Link from 'next/link';
+import Menu from '@/components/layout/Menu';
 
 export default function Home() {
   return (
-    <main style={{width: '375px', border: '1px solid #999999', margin: '0 auto' }}>
-      <section>
+    <main style={{ width: '375px', border: '1px solid #999999', margin: '0 auto' }}>
         <MainHeader />
-      </section>
-      <section style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap:'3rem', padding:'5rem 0'}}>
+      <section
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          gap: '3rem',
+          padding: '5rem 0'
+        }}
+      >
         <Link href="/cart">
           <Image src={buttonImage} alt="" />
           장보기
@@ -19,6 +28,7 @@ export default function Home() {
           냉장고
         </Link>
       </section>
+      <Menu/>
     </main>
   );
 }
