8de4c492fd813dc73883a7e9c07df7448479f0f7
kimsohyeon <aotoyae@gmail.com>
2024-07-18 00:21:17 +0900

    [feat] #33 대분류 배경 이미지 추가

--- src/app/(menuBar)/fridge/item/page.tsx
+++ src/app/(menuBar)/fridge/item/page.tsx
@@ -1,42 +1,44 @@
 import './style.css';
+import Image from 'next/image';
+import button from '@/assets/button/button_orange.png';
 
 const FridgeAddPage = () => {
   return (
     <ul className="largeCategoryList">
       <li>
-        <div>img</div>
+        <Image src={button} alt="버튼 배경 이미지" />
         <h4>야채</h4>
       </li>
       <li>
-        <div>img</div>
+        <Image src={button} alt="버튼 배경 이미지" />
         <h4>과일</h4>
       </li>
       <li>
-        <div>img</div>
+        <Image src={button} alt="버튼 배경 이미지" />
         <h4>육류</h4>
       </li>
       <li>
-        <div>img</div>
+        <Image src={button} alt="버튼 배경 이미지" />
         <h4>해물</h4>
       </li>
       <li>
-        <div>img</div>
+        <Image src={button} alt="버튼 배경 이미지" />
         <h4>곡류</h4>
       </li>
       <li>
-        <div>img</div>
+        <Image src={button} alt="버튼 배경 이미지" />
         <h4>조미료</h4>
       </li>
       <li>
-        <div>img</div>
+        <Image src={button} alt="버튼 배경 이미지" />
         <h4>가공식품</h4>
       </li>
       <li>
-        <div>img</div>
+        <Image src={button} alt="버튼 배경 이미지" />
         <h4>음료</h4>
       </li>
       <li>
-        <div>img</div>
+        <Image src={button} alt="버튼 배경 이미지" />
         <h4>기타</h4>
       </li>
     </ul>
