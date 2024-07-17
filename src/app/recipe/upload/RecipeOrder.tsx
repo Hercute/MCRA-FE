a731d5243d990b24f5818e029619c83cab215355
Kim CG <cjfrbs1256@gmail.com>
2024-07-17 16:37:06 +0900

    [feat]#20: 레시피올리기-사진첨부

--- src/app/recipe/upload/RecipeOrder.tsx
+++ src/app/recipe/upload/RecipeOrder.tsx
@@ -1,65 +1,64 @@
 'use client';
-import React, { useRef, useState } from 'react';
-import { IoBookOutline } from 'react-icons/io5';
-import { AiOutlinePicture } from 'react-icons/ai';
-import { GoPlusCircle } from 'react-icons/go';
+import React, { ChangeEvent } from 'react';
 import { useRecoilState } from 'recoil';
 import { orderPagesState } from '@/recoil/atoms/recoilState';
-
-import './upload.css';
+import { OrderPage } from '@/types/recipe';
+import { AiOutlinePicture } from 'react-icons/ai';
+import { GoPlusCircle } from 'react-icons/go';
 import Image from 'next/image';
 
-const RecipeOrder: React.FC = () => {
-  const [recipePage, setRecipePage] = useRecoilState(orderPagesState);
-  const fileInputRef = useRef<HTMLInputElement | null>(null);
-  const [filePreview, setFilePreview] = useState<string | null>(null);
+const RecipeComponent: React.FC = () => {
+  const [recipePages, setRecipePages] = useRecoilState<OrderPage[]>(orderPagesState);
 
-  const handleDivClick = () => {
-    if (fileInputRef.current) {
-      fileInputRef.current.click();
-    }
+  const handleAddPage = () => {
+    const newPage: OrderPage = {
+      id: Date.now(),
+      description: '',
+      file: undefined,
+      filePreview: undefined,
+      showDeleteButton: true
+    };
+    setRecipePages([...recipePages, newPage]);
   };
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>, pageId: number) => {
     const file = e.target.files?.[0];
     if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setFilePreview(reader.result as string);
-      };
-      reader.readAsDataURL(file);
+      const filePreview = URL.createObjectURL(file);
+      setRecipePages(recipePages.map((page) => (page.id === pageId ? { ...page, file, filePreview } : page)));
+      console.log(recipePages.map((page) => page));
     }
   };
-  const handleAddPage = () => {
-    setRecipePage([
-      ...recipePage,
-      { id: recipePage.length + 1, description: '', file: undefined, showDeleteButton: true }
-    ]);
+
+  const handleDescription = (pageId: number, description: string) => {
+    setRecipePages(recipePages.map((page) => (page.id === pageId ? { ...page, description } : page)));
   };
-  const handleDeleteOrder = (id: number) => {
-    console.log(id);
-    const updateOrder = recipePage.filter((page) => page.id !== id);
-    setRecipePage(updateOrder);
-    console.log(recipePage);
+
+  const handleDeleteOrder = (pageId: number) => {
+    setRecipePages(recipePages.filter((page) => page.id !== pageId));
   };
-  const handleDescription = (id: number, value: string) => {
-    setRecipePage((prevPages) => prevPages.map((page) => (page.id === id ? { ...page, description: value } : page)));
+
+  const handleDivClick = (pageId: number) => {
+    document.getElementById(`fileInput-${pageId}`)?.click();
   };
+
   return (
-    <section className="recipeSection">
-      <div className="uploadDiv">
-        <IoBookOutline size={27} />
-        <span>조리순서</span>
-      </div>
-      {recipePage.map((page, index) => (
+    <div>
+      {recipePages.map((page, index) => (
         <div className="orderItem" key={page.id}>
-          <div className="orderFileBox" onClick={handleDivClick}>
-            {filePreview ? (
-              <Image src={filePreview} alt={page.description} className="uploadedImage" width={80} height={80} />
+          <div className="orderFileBox" onClick={() => handleDivClick(page.id)}>
+            {page.filePreview ? (
+              <Image src={page.filePreview} alt={page.description} className="uploadedImage" width={80} height={80} />
             ) : (
               <AiOutlinePicture size={36} />
             )}
-
-            <input type="file" className="orderFile" ref={fileInputRef} onChange={handleFileChange} />
+            <input
+              type="file"
+              className="orderFile"
+              id={`fileInput-${page.id}`}
+              style={{ display: 'none' }}
+              onChange={(e) => handleFileChange(e, page.id)}
+            />
           </div>
           <div className="recipeExplanation">
             <span>{index + 1}</span>
@@ -80,8 +79,8 @@ const RecipeOrder: React.FC = () => {
       <div className="addPageButton" onClick={handleAddPage}>
         <GoPlusCircle size={24} />
       </div>
-    </section>
+    </div>
   );
 };
 
-export default RecipeOrder;
+export default RecipeComponent;
