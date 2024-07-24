ebd85253b13e2b3570dc9ee7a80567a4d22e6c16
kimsohyeon <aotoyae@gmail.com>
2024-07-24 19:22:06 +0900

    [feat] #33 안내 문구 추가 및 스타일 수정

--- src/app/(menuBar)/fridge/item/page.tsx
+++ src/app/(menuBar)/fridge/item/page.tsx
@@ -2,35 +2,47 @@ import './style.css';
 
 const FridgeAddPage = () => {
   return (
-    <ul className="largeCategoryList">
-      <li>
-        <h4>야채</h4>
-      </li>
-      <li>
-        <h4>과일</h4>
-      </li>
-      <li>
-        <h4>육류</h4>
-      </li>
-      <li>
-        <h4>해물</h4>
-      </li>
-      <li>
-        <h4>곡류</h4>
-      </li>
-      <li>
-        <h4>조미료</h4>
-      </li>
-      <li>
-        <h4>가공식품</h4>
-      </li>
-      <li>
-        <h4>음료</h4>
-      </li>
-      <li>
-        <h4>기타</h4>
-      </li>
-    </ul>
+    <section className="largeCategorySection">
+      <ul className="largeCategoryList">
+        <li>
+          <div></div>
+          <h4>야채</h4>
+        </li>
+        <li>
+          <div></div>
+          <h4>과일</h4>
+        </li>
+        <li>
+          <div></div>
+          <h4>육류</h4>
+        </li>
+        <li>
+          <div></div>
+          <h4>해물</h4>
+        </li>
+        <li>
+          <div></div>
+          <h4>곡류</h4>
+        </li>
+        <li>
+          <div></div>
+          <h4>조미료</h4>
+        </li>
+        <li>
+          <div></div>
+          <h4>가공식품</h4>
+        </li>
+        <li>
+          <div></div>
+          <h4>음료</h4>
+        </li>
+        <li>
+          <div></div>
+          <h4>기타</h4>
+        </li>
+      </ul>
+      <h3>추가할 재료의 종류를 선택해 주세요.</h3>
+    </section>
   );
 };
 
